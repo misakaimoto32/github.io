c477c2d410d0ea0c5ca18eb2059635baf3ad6169
misakaimoto32 <83196005+misakaimoto32@users.noreply.github.com>
2021-05-01 16:55:44 +0800

    增加对冲计算

--- Calculator/js/console.js
+++ Calculator/js/console.js
@@ -4,8 +4,7 @@ fetch(`https://api.exchangerate-api.com/v4/latest/${rate1}`).then(res=>res.json(
 {
 	const rate=data.rates[rate2];
 	switchrate=rate;
-	console.log(switchrate);
 	document.getElementById("money2").value=switchrate*document.getElementById("money1").value;
 	
-});return rate;
+});
 }
